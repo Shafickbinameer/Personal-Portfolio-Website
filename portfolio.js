54ed8324a6ee8cf861a42bddc69f21f8aaa8f502
Shafickbinameer <shafickbinameer@gmail.com>
2024-09-07 19:20:32 +0530

    personal portfolio website

---
+++ portfolio.js
@@ -0,0 +1,60 @@
+
+//creating an slider effect
+const productContainer = document.querySelectorAll('.port-slider');
+const nxtbtn = document.querySelectorAll('.next');
+const prebnt = document.querySelectorAll('.prev');
+
+productContainer.forEach((item, i) => {
+    let contentdimenssion = item.getBoundingClientRect();
+    let contentwidth = contentdimenssion.width;
+
+
+    nxtbtn[i].addEventListener('click', () => {
+        item.scrollLeft += contentwidth;
+    })
+
+    prebnt[i].addEventListener('click', () => {
+        item.scrollLeft -= contentwidth;
+    })
+})
+
+
+//media queries
+
+var menu = document.querySelector('.navmob');
+if (menu.addEventListener('mouseover', () => {
+    var navbar = document.querySelector(".port-nav");
+    var screenwidth = screen.availWidth;
+    if (screenwidth <= 600) {
+        navbar.style.display = "flex";
+        navbar.style.transition = ".3s";
+    } else {
+        navbar.style.display = "none";
+    }
+}));
+else if (menu.addEventListener('mouseout', () => {
+    var navbar = document.querySelector(".port-nav");
+
+    navbar.style.display = "none";
+}));
+
+var screenwidth = screen.availWidth;
+if (screenwidth <= 600) {
+    const productContainer = document.querySelectorAll('.port-slider');
+    const nxtbtn = document.querySelectorAll('.next');
+    const prebnt = document.querySelectorAll('.prev');
+
+    productContainer.forEach((item, i) => {
+        let contentdimenssion = item.getBoundingClientRect();
+        let contentwidth = contentdimenssion.width;
+
+
+        nxtbtn[i].addEventListener('click', () => {
+            item.scrollLeft += contentwidth;
+        })
+
+        prebnt[i].addEventListener('click', () => {
+            item.scrollLeft -= contentwidth;
+        })
+    })
+}
